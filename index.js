0b5d9c9aba7f3acdd47c2ff3c2a30b63445cd73a
Akila-Kavisinghe <38820066+Akila-Kavisinghe@users.noreply.github.com>
2018-12-20 21:17:13 -0500

    Adding shit
    Added modules and  added the original .js file back

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const Discord = require ('discord.js');
 const bot = new Discord.Client();
-//hello
+
 
 bot.on('message', (message) => {
 
-    message = message.split();
-    if(message.indexOf('ping') > -1){
+    if(message.content == 'ping'){
         message.reply('pong');
     }
 
 });
-
 
 bot.login('NTI1NDc2ODI3MjY3ODU4NDM3.Dv3NdA.yjt742wYZam6k8HkmgW-zneF4-Y');
